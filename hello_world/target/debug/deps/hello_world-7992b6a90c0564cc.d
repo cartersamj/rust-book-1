be3b52c187ae7953cf6eeb62f88ee37d0a7033bf
cartersamj <sammahcarter@gmail.com>
2022-10-07 21:16:00 -0700

    made manual file

--- hello_world/target/debug/deps/hello_world-7992b6a90c0564cc.d
+++
@@ -1,5 +0,0 @@
-/home/sammahcarter/rust/rust-book-1/hello_world/target/debug/deps/hello_world-7992b6a90c0564cc.rmeta: src/main.rs
-
-/home/sammahcarter/rust/rust-book-1/hello_world/target/debug/deps/hello_world-7992b6a90c0564cc.d: src/main.rs
-
-src/main.rs:
