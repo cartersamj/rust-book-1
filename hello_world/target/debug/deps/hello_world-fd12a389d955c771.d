b14e8d785e2f0f6cc8b28e46f7377a6342e5809c
cartersamj <sammahcarter@gmail.com>
2022-10-07 21:13:16 -0700

    tried cargo new

---
+++ hello_world/target/debug/deps/hello_world-fd12a389d955c771.d
@@ -0,0 +1,5 @@
+/home/sammahcarter/rust/rust-book-1/hello_world/target/debug/deps/hello_world-fd12a389d955c771.rmeta: src/main.rs
+
+/home/sammahcarter/rust/rust-book-1/hello_world/target/debug/deps/hello_world-fd12a389d955c771.d: src/main.rs
+
+src/main.rs:
